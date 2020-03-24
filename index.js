c520b1a5fdd1d32d6037d82cabe60a0b73528b1b
Isaiah Cruz <isaiahgcruz@gmail.com>
2020-03-25 00:40:43 +0800

    config: Add eslint and prettier

---
+++ index.js
@@ -0,0 +1,17 @@
+// TODO: Remove this
+// Just to test eslint and prettier works
+
+const add = (thisIsAVeryLongVariable, andAnotherVeryLongvariable) => {
+  if (
+    thisIsAVeryLongVariable &&
+    andAnotherVeryLongvariable &&
+    !!andAnotherVeryLongvariable &&
+    !!thisIsAVeryLongVariable
+  ) {
+    return thisIsAVeryLongVariable + andAnotherVeryLongvariable;
+  }
+
+  return 0;
+};
+
+export default add;
