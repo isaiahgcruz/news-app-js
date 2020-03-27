1c7f7fe69d06424d6e61dac8c17acf95821df826
Isaiah Cruz <isaiahgcruz@gmail.com>
2020-03-27 22:52:16 +0800

    feat(client): Add pagination

--- packages/client/src/components/Source/Articles/Articles.jsx
+++ packages/client/src/components/Source/Articles/Articles.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, makeStyles } from '@material-ui/core';
+import { Box, makeStyles, Divider } from '@material-ui/core';
 import ArticleItem from './ArticleItem';
+import Pagination from '../../Pagination';
 import useNewsContext from '../../../contexts/NewsContext/useNewsContext';
 
 const useStyles = makeStyles({
@@ -17,31 +18,50 @@ const Articles = () => {
 
   const {
     state: {
-      source: { data, isFetching },
+      source: { data, isFetching, page, size = 10, length = 25, id },
     },
+    actions: { fetchSource },
   } = useNewsContext();
 
+  const handleOnChangePage = (newPage) => {
+    fetchSource(id, newPage);
+  };
+
   return (
-    <Box
-      className={classes.root}
-      display="flex"
-      flexDirection="row"
-      flexWrap="wrap"
-      p={1}
-      m={1}
-    >
-      {isFetching
-        ? loadingArray.map((val) => <ArticleItem key={val} isSkeleton />)
-        : data.map(({ id, image, title, date, description }) => (
-            <ArticleItem
-              key={id}
-              image={image}
-              title={title}
-              date={date}
-              description={description}
-            />
-          ))}
-    </Box>
+    <>
+      <Box
+        className={classes.root}
+        display="flex"
+        flexDirection="row"
+        flexWrap="wrap"
+        p={1}
+        m={1}
+      >
+        {isFetching
+          ? loadingArray.map((val) => <ArticleItem key={val} isSkeleton />)
+          : data.map(({ id: itemId, image, title, date, description }) => (
+              <ArticleItem
+                key={itemId}
+                image={image}
+                title={title}
+                date={date}
+                description={description}
+              />
+            ))}
+      </Box>
+      {(!!data.length || isFetching) && (
+        <>
+          <Divider />
+          <Pagination
+            disabled={isFetching}
+            page={page}
+            size={size}
+            length={length}
+            onChange={handleOnChangePage}
+          />
+        </>
+      )}
+    </>
   );
 };
 
